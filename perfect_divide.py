43db902d88cb482a1ade15fe231df43cbab17bd4
efrat <efratk464@gmail.com>
2022-04-10 23:32:33 +0300

    All repairs

--- perfect_divide.py
+++ perfect_divide.py
@@ -1,15 +1,17 @@
-"""
- generator that find a perfect number
-"""
+
 def pefect_num():
-    n=2
-    while n:
-        sum1 = 0
-        l = [i for i in range(1, n) if(n % i == 0)]
-        sum1 = sum(l)
-        if sum1 == n:
-            yield sum1
-        n=n+1
+    """
+     generator that find a perfect number-
+     A perfect number is a number whose division (without 1) is equal to itself
+    """
+    counter = 2 #1 divides each number so we'll start with 2
+    while counter:
+        temp_sum = 0
+        list_of_divide = [temp for temp in range(1, counter) if(counter % temp == 0)]
+        temp_sum = sum(list_of_divide)
+        if temp_sum == counter:
+            yield temp_sum
+        counter = counter+1
 
 if __name__=="__main__":
     num_perfect=pefect_num()
--- tar5.4_tool.py
+++ tar5.4_tool.py
@@ -1,35 +1,37 @@
-"""
-    fanction that return thee min's lengh of the argument
-"""
+
 def find_min(*interleave):
-    min =interleave[0]
-    min_len=len(interleave[0])
+    """
+       function that return the min length of the argument
+    """
+    min_lenght = interleave[0]
+    min_len = len(interleave[0])
     for item in interleave:
-        if min_len>len(item):
-            min=item
-    return min
+        if min_len > len(item):
+            min_lenght = item
+    return min_lenght
 
 """
     A function that receives several lists and returns one list in the requested order
+    For example for [1,2,3] [#,*,+] return- [1,#,2,*,3,+]
 """
-def interleave(*interleave):
-    count=0
-    intr = []
-    for i in  interleave:
-        intr += [i]
-    while intr:
-        this_item = find_min(*intr)
-        for i in range(len(this_item)-count):
-            l=[]
-            for item in intr:
-                l+= [item[count]]
-            yield l
-            count += 1
-        temp=intr
-        intr=[]
+def interleave(*interleave): #interleave- A variable that can have several lists
+    count_iteration = 0
+    rest_interleave = []
+    for i in interleave:
+        rest_interleave += [i]
+    while rest_interleave:
+        current_item = find_min(*rest_interleave)
+        for i in range(len(current_item)-count_iteration):
+            listt = []
+            for item in rest_interleave:
+                listt += [item[count_iteration]]
+            yield listt
+            count_iteration += 1
+        temp = rest_interleave
+        rest_interleave = []
         for it in temp:
-            if not len(it) == len(this_item):
-                intr +=[it]
+            if not len(it) == len(current_item):
+                rest_interleave += [it]
 
 
 if __name__=="__main__":
